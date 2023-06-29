1f53c0d863fed1aea1a5391c21b3cca51506a165
prynskaf <prince.kaf12@gmail.com>
2023-06-29 15:46:28 +0200

    updated

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon')
+const apiKey = "afe803993451d8ac7dad61772b10adb8";
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
 
-const weather = document.querySelector('.weather');
-const btn = document.querySelector('button');
+async function checkWeather(city) {
+  const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
+  const data = await response.json();
+  console.log(data);
+
+  document.querySelector('.city').innerHTML = data.city.name;
+  document.querySelector('.temp').innerHTML = Math.round(data.list[0].main.temp) + '°C';
+  document.querySelector('.humidity').innerHTML = data.list[0].main.humidity + "%";
+  document.querySelector('.wind').innerHTML = data.list[0].wind.speed + ' km/h';
 
 
 
-btn.addEventListener('click', () => {
-  openWeatherWithDelay();
-});
-
-document.addEventListener('keydown', event => {
-  if (event.key === 'Enter') {
-    openWeatherWithDelay();
-   
+  //if statement to change the weather images
+  if(data.weather[0].main == 'Clouds') {
+    weatherIcon.src = 'images/clouds.png';
+    console.log('it is cloudy');
   }
-});
-
-function openWeatherWithDelay() {
-  setTimeout(() => {
-    weather.style.display = 'block';
-    weather.style.transition = 'all 0.7s ease';
-    console.log('It has been clicked');
-  }, 1000); // Delay of 2000 milliseconds (2 seconds)
+  else if(data.weather[0].main == 'Clear') {
+    weatherIcon.src = 'images/clear.png';
+    console.log('it is clear');
+  }
+  else if(data.weather[0].main == 'Rain') {
+    weatherIcon.src = 'images/rain.png';
+    console.log('it is Rainy');
+  }
+  else if(data.weather[0].main == 'Drizzle') {
+    weatherIcon.src = 'images/drizzle.png';
+    console.log('it is Drizzling');
+  }
+  else if(data.weather[0].main == 'Snow') {
+    weatherIcon.src = 'images/snow.png';
+    console.log('it is snowing');
+  }
 }
 
+searchBtn.addEventListener('click', () => {
+  checkWeather(searchBox.value);
+  console.log('working fine');
+  document.querySelector('.weather').style.display = 'block';
+});
