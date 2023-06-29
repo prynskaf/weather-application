c5df34182bda999a40a1f315d9178d00ab949f75
prynskaf <prince.kaf12@gmail.com>
2023-06-29 09:09:22 +0200

    updated files for this morning

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
-// selected element 
-const cityName = document.querySelector('#cityNameInput');
-const submitBtn = document.querySelector('#submitButton');
-const resultsContainer = document.querySelector('.resultsContainer');
-const  result = document.querySelector('.resultSelect')
 
- submitBtn.addEventListener('click', async () => {
-    //get the values entered in the name input and country selected field
-    const city = cityName.value
-    console.log( 'city:' , city)
+const weather = document.querySelector('.weather');
+const btn = document.querySelector('button');
 
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=afe803993451d8ac7dad61772b10adb8`;
 
-fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    result.textContent = `
-    City: ${data.name}
-    country: ${data.sys.country}
-    temp: ${data.main.temp}
-    weather = ${data.weather[0]["main"]}
-    `;
-
-
-    console.log( `Country: ${country}, City: ${name}, weather: ${weather}, temp: ${temp}`
-    );
-    
-    //adding the data to my results
-  })
-  .catch((error) => console.error(error));
-
-  //adding all the results into the result div
-//   result.textContent = `
-//   name: ${data.name}
-//   country: ${data.country}
-//   temp: ${data.main.temp}
-//   weather = ${data.weather[0]["main"]}
-//   `
+btn.addEventListener('click', () => {
+  openWeatherWithDelay();
 });
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    openWeatherWithDelay();
+   
+  }
+});
+
+function openWeatherWithDelay() {
+  setTimeout(() => {
+    weather.style.display = 'block';
+    weather.style.transition = 'all 0.7s ease';
+    console.log('It has been clicked');
+  }, 1000); // Delay of 2000 milliseconds (2 seconds)
+}
 
